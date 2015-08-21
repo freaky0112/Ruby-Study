6f43d0cd387537180c639f609a38c10768538d4a
freaky0112 <freakoutelf@gmail.com>
2015-08-21 09:51:55 +0800

    Change the algorithm about the problem 5

--- ProjectEular/5Smallest multiple.rb
+++ ProjectEular/5Smallest multiple.rb
@@ -1,17 +1,35 @@
-def Divisible(limit,number)
-	status=true
-	while limit>1
-		if number%limit!=0
-			status=false
-			break
+def defPrimes(limit)
+	primes=Array.new(limit) { |i| i=true  }
+	i=2
+	while i<limit
+		if primes[i]
+			n=i
+			while i*n<limit
+				primes[i*n]=false
+				n+=1
+			end
 		end
-		limit-=1
+		i+=1	
 	end
-	return status
+	return primes
 end
 
-i=20
-while !Divisible(20,i)
-	i+=20
+limit=20
+primes=defPrimes(limit)
+i=2
+array=Hash.new
+while i<limit	
+	if primes[i]
+		n=0
+		while i**n<=20
+			n+=1
+		end
+		array.store(i,n-1)
+	end
+	i+=1
 end
-puts i
+sum=1
+array.each { |key,value| 
+	sum*=key**value
+ }
+ puts sum
